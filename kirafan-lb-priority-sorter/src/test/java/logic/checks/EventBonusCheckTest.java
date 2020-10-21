4430daee46b8b8ba3e72ebd517791444d2f7c1f2
joq tyyppi <eero.ojala@helsinki.fi>
2020-10-21 18:26:29 +0300

    Refactored PersonalPreferenceTest and EventBonusCheck

--- kirafan-lb-priority-sorter/src/test/java/logic/checks/EventBonusCheckTest.java
+++ kirafan-lb-priority-sorter/src/test/java/logic/checks/EventBonusCheckTest.java
@@ -16,57 +16,63 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EventBonusCheckTest {
     private EventBonusCheck check;
-    private GameCharacter c1;
-    private GameCharacter c2;
-    private GameCharacter c3;
-    private GameCharacter c4;
+    private GameCharacter cA;
+    private GameCharacter cB;
+    private GameCharacter cC;
+    private GameCharacter cD;
     private GameEvent event; // c1 and c2 are bonus characters
 
     public EventBonusCheckTest() {
         Series series = new Series("series", CreaStatus.COMPLETE);
-        c1 = new GameCharacter.Builder("c1", series, CharacterElement.SUN, CharacterClass.PRIEST).build();
-        c2 = new GameCharacter.Builder("c2", series, CharacterElement.SUN, CharacterClass.PRIEST).build();
-        c3 = new GameCharacter.Builder("c3", series, CharacterElement.SUN, CharacterClass.PRIEST).build();
-        c4 = new GameCharacter.Builder("c4", series, CharacterElement.SUN, CharacterClass.PRIEST).build();
+        cA = new GameCharacter.Builder("c1", series, CharacterElement.SUN, CharacterClass.PRIEST).build();
+        cB = new GameCharacter.Builder("c2", series, CharacterElement.SUN, CharacterClass.PRIEST).build();
+        cC = new GameCharacter.Builder("c3", series, CharacterElement.SUN, CharacterClass.PRIEST).build();
+        cD = new GameCharacter.Builder("c4", series, CharacterElement.SUN, CharacterClass.PRIEST).build();
 
         event = new GameEvent();
-        List<GameCharacter> bonusCharacters = new ArrayList<>(Arrays.asList(c1, c2));
+        List<GameCharacter> bonusCharacters = new ArrayList<>(Arrays.asList(cA, cB));
         event.setBonusCharacters(bonusCharacters);
 
         check = new EventBonusCheck(new ArrayList<GameCharacter>(), event);
     }
 
-    @Test
-    public void check_returnsZeroWhenEventIsNull() {
-        EventBonusCheck anotherCheck = new EventBonusCheck(new ArrayList<GameCharacter>(), null);
-        assertEquals(0, anotherCheck.check(c2, c3));
-    }
 
     @Test
     public void check_returnsNegativeOneWhenC1IsBonusAndC2IsNot() {
-        assertEquals(-1, check.check(c1, c3));
-        assertEquals(-1, check.check(c1, c4));
-        assertEquals(-1, check.check(c2, c3));
-        assertEquals(-1, check.check(c2, c4));
+        assertNotEquals(-1, check.check(cA, cB)); // both characters are bonus
+        assertNotEquals(-1, check.check(cD, cA)); // cD is not bonus, cA is bonus
+        assertEquals(-1, check.check(cA, cC)); // cA is bonus,  cC is not bonus
+        assertNotEquals(-1, check.check(cC, cD)); // Neither character is bonus
     }
 
     @Test
     public void check_returnsOneWhenC2IsBonusAndC1IsNot() {
-        assertEquals(1, check.check(c3, c1));
-        assertEquals(1, check.check(c3, c2));
-        assertEquals(1, check.check(c4, c1));
-        assertEquals(1, check.check(c4, c2));
+        assertNotEquals(1, check.check(cA, cB)); // both characters are bonus
+        assertEquals(1, check.check(cD, cA)); // cD is not bonus, cA is bonus
+        assertNotEquals(1, check.check(cA, cC)); // cA is bonus,  cC is not bonus
+        assertNotEquals(1, check.check(cC, cD)); // Neither character is bonus
     }
 
     @Test
-    public void check_returnsZeroWhenBothCharactersAreBonus() {
-        assertEquals(0, check.check(c1, c2));
-        assertEquals(0, check.check(c2, c1));
+    public void check_returnsZero_WhenEventIsNull() {
+        EventBonusCheck anotherCheck = new EventBonusCheck(new ArrayList<GameCharacter>(), null);
+        assertEquals(0, anotherCheck.check(cA, cB));
+        assertEquals(0, anotherCheck.check(cA, cC));
+        assertEquals(0, anotherCheck.check(cA, cD));
+        assertEquals(0, anotherCheck.check(cB, cC));
+        assertEquals(0, anotherCheck.check(cB, cD));
+        assertEquals(0, anotherCheck.check(cC, cD));
     }
 
     @Test
-    public void check_returnsZeroWhenNeitherCharacterIsBonus() {
-        assertEquals(0, check.check(c3, c4));
-        assertEquals(0, check.check(c4, c3));
+    public void check_returnsZero_whenBothCharactersAreBonus() {
+        assertEquals(0, check.check(cA, cB));
+        assertEquals(0, check.check(cB, cA));
+    }
+
+    @Test
+    public void check_returns_zeroWhenNeitherCharacterIsBonus() {
+        assertEquals(0, check.check(cC, cD));
+        assertEquals(0, check.check(cD, cC));
     }
 }
--- kirafan-lb-priority-sorter/src/test/java/logic/checks/PersonalPreferenceCheckTest.java
+++ kirafan-lb-priority-sorter/src/test/java/logic/checks/PersonalPreferenceCheckTest.java
@@ -11,37 +11,37 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PersonalPreferenceCheckTest {
     private PersonalPreferenceCheck check;
-    private GameCharacter c1;
-    private GameCharacter c2;
-    private GameCharacter c3;
-    private GameCharacter c4;
-    private GameCharacter c5;
-    private GameCharacter c6;
-    
+    private GameCharacter cA;
+    private GameCharacter cB;
+    private GameCharacter cC;
+    private GameCharacter cD;
+    private GameCharacter cE;
+    private GameCharacter cF;
+
     public PersonalPreferenceCheckTest() {
         Series s = new Series("s", CreaStatus.NONE);
 
-        c1 = new GameCharacter.Builder("c1", s, CharacterElement.EARTH, CharacterClass.WARRIOR)
+        cA = new GameCharacter.Builder("c1", s, CharacterElement.EARTH, CharacterClass.WARRIOR)
                 .withPersonalPreference(9)
                 .build();
 
-        c2 = new GameCharacter.Builder("c2", s, CharacterElement.EARTH, CharacterClass.WARRIOR)
+        cB = new GameCharacter.Builder("c2", s, CharacterElement.EARTH, CharacterClass.WARRIOR)
                 .withPersonalPreference(8)
                 .build();
 
-        c3 = new GameCharacter.Builder("c3", s, CharacterElement.FIRE, CharacterClass.ALCHEMIST)
+        cC = new GameCharacter.Builder("c3", s, CharacterElement.FIRE, CharacterClass.ALCHEMIST)
                 .withPersonalPreference(9)
                 .build();
 
-        c4 = new GameCharacter.Builder("c4", s, CharacterElement.FIRE, CharacterClass.ALCHEMIST)
+        cD = new GameCharacter.Builder("c4", s, CharacterElement.FIRE, CharacterClass.ALCHEMIST)
                 .withPersonalPreference(7)
                 .build();
 
-        c5 = new GameCharacter.Builder("c5", s, CharacterElement.FIRE, CharacterClass.ALCHEMIST)
+        cE = new GameCharacter.Builder("c5", s, CharacterElement.FIRE, CharacterClass.ALCHEMIST)
                 .withPersonalPreference(5)
                 .build();
 
-        c6 = new GameCharacter.Builder("c6", s, CharacterElement.FIRE, CharacterClass.ALCHEMIST)
+        cF = new GameCharacter.Builder("c6", s, CharacterElement.FIRE, CharacterClass.ALCHEMIST)
                 .withPersonalPreference(7)
                 .build();
 
@@ -50,57 +50,57 @@ class PersonalPreferenceCheckTest {
 
     @Test
     public void check_returnsNegativeOne_whenC1HasHigherPreferenceWhichIsInRange() {
-        // c1 has a priority of 9 which is within the given range (>=8)
-        assertEquals(-1, check.check(c1, c2)); // c2 has a priority of 8 which is within range and lower than c1's
-        assertNotEquals(-1, check.check(c1, c3)); // c3 has a priority of 9 which is within range and equal to c1's
-        assertEquals(-1, check.check(c1, c4)); // c4 has a priority of 7 which is NOT  within range and lower than c1's
+        // cA has a priority of 9 which is within the given range (>=8)
+        assertEquals(-1, check.check(cA, cB)); // cB has a priority of 8 which is within range and lower than cA's
+        assertNotEquals(-1, check.check(cA, cC)); // cC has a priority of 9 which is within range and equal to cA's
+        assertEquals(-1, check.check(cA, cD)); // cD has a priority of 7 which is NOT within range and lower than cA's
 
-        // c2 has a priority of 8 which is the lowest value within the given range
-        assertNotEquals(-1, check.check(c2, c1)); // c1 has a priority of 9 which is within range and higher than c2's
-        assertEquals(-1, check.check(c2, c4)); // c4 has a priority of 7 which is NOT within range and lower than c2's
+        // cB has a priority of 8 which is the lowest value within the given range
+        assertNotEquals(-1, check.check(cB, cA)); // cA has a priority of 9 which is within range and higher than cB's
+        assertEquals(-1, check.check(cB, cD)); // cD has a priority of 7 which is NOT within range and lower than cB's
 
-        // c4 has a priority of 7 which is NOT within the given range
-        assertNotEquals(-1, check.check(c4, c1)); // c1 has a priority of 9 which is within range and higher than c1's.
-        assertNotEquals(-1, check.check(c4, c5)); // c5 has a priority of 5 which is NOT within range and lower than c4's
-        assertNotEquals(-1, check.check(c4, c6)); // c6 has a priority of 7 which is NOT within range and equal to c4's
+        // cD has a priority of 7 which is NOT within the given range
+        assertNotEquals(-1, check.check(cD, cA)); // cA has a priority of 9 which is within range and higher than cD's.
+        assertNotEquals(-1, check.check(cD, cE)); // cE has a priority of 5 which is NOT within range and lower than cD's
+        assertNotEquals(-1, check.check(cD, cF)); // cF has a priority of 7 which is NOT within range and equal to cD's
 
-        // c5 has a priority of 5 which is NOT within the given range
-        assertNotEquals(-1, check.check(c5, c4)); // c4 has a priority of 7 which is NOT within range and higher than c5's
+        // cE has a priority of 5 which is NOT within the given range
+        assertNotEquals(-1, check.check(cE, cD)); // cD has a priority of 7 which is NOT within range and higher than cE's
     }
 
     @Test
     public void check_returnsOne_whenC2HasHigherPreferenceWhichIsInRange() {
-        // c1 has a priority of 9 which is within the given range (>=8)
-        assertNotEquals(1, check.check(c1, c2)); // c2 has a priority of 8 which is within range and lower than c1's
-        assertNotEquals(1, check.check(c1, c3)); // c3 has a priority of 9 which is within range and equal to c1's
-        assertNotEquals(1, check.check(c1, c4)); // c4 has a priority of 7 which is NOT  within range and lower than c1's
+        // cA has a priority of 9 which is within the given range (>=8)
+        assertNotEquals(1, check.check(cA, cB)); // cB has a priority of 8 which is within range and lower than cA's
+        assertNotEquals(1, check.check(cA, cC)); // cC has a priority of 9 which is within range and equal to cA's
+        assertNotEquals(1, check.check(cA, cD));  // cD has a priority of 7 which is NOT within range and lower than cA's
 
-        // c2 has a priority of 8 which is the lowest value within the given range
-        assertEquals(1, check.check(c2, c1)); // c1 has a priority of 9 which is within range and higher than c2's
-        assertNotEquals(1, check.check(c2, c4)); // c4 has a priority of 7 which is NOT within range and lower than c2's
+        // cB has a priority of 8 which is the lowest value within the given range
+        assertEquals(1, check.check(cB, cA)); // cA has a priority of 9 which is within range and higher than cB's
+        assertNotEquals(1, check.check(cB, cD)); // cD has a priority of 7 which is NOT within range and lower than cB's
 
-        // c4 has a priority of 7 which is NOT within the given range
-        assertEquals(1, check.check(c4, c1)); // c1 has a priority of 9 which is within range and higher than c4's.
-        assertNotEquals(1, check.check(c4, c5)); // c5 has a priority of 5 which is NOT within range and lower than c4's
-        assertNotEquals(1, check.check(c4, c6)); // c6 has a priority of 7 which is NOT within range and equal to c4's
+        // cD has a priority of 7 which is NOT within the given range
+        assertEquals(1, check.check(cD, cA)); // cA has a priority of 9 which is within range and higher than cD's.
+        assertNotEquals(1, check.check(cD, cE)); // cE has a priority of 5 which is NOT within range and lower than cD's
+        assertNotEquals(1, check.check(cD, cF)); // cF has a priority of 7 which is NOT within range and equal to cD's
 
-        // c5 has a priority of 5 which is NOT within the given range
-        assertNotEquals(1, check.check(c5, c4)); // c4 has a priority of 7 which is NOT within range and higher than c5's
+        // cE has a priority of 5 which is NOT within the given range
+        assertNotEquals(1, check.check(cE, cD)); // cD has a priority of 7 which is NOT within range and higher than cE's
     }
 
     @Test
     public void check_returnsZero_whenBothCharactersHaveEqualPreferenceWhichIsInRange() {
-        assertEquals(0, check.check(c1, c3)); // Both have preference of 9 is within range
+        assertEquals(0, check.check(cA, cC)); // Both have preference of 9 is within range
     }
 
     @Test
     public void check_returnsZero_whenBothCharactersPreferenceIsNotInRange() {
-        // c4 has a priority of 7 which is NOT within the given range
-        assertEquals(0, check.check(c4, c5)); // c5 has a preference of 5 which is NOT within range and lower than c4's
-        assertEquals(0, check.check(c4, c6)); // c6 has a preference of 7 which is NOT within range and equal to c4's
+        // cD has a priority of 7 which is NOT within the given range
+        assertEquals(0, check.check(cD, cE)); // cE has a preference of 5 which is NOT within range and lower than cD's
+        assertEquals(0, check.check(cD, cF)); // cF has a preference of 7 which is NOT within range and equal to cD's
 
-        // c5 has a priority of 5 which is NOT within the given range
-        assertEquals(0, check.check(c5, c4)); // c4 has a preference of 7 which is NOT within the range and higher than c5's
+        // cE has a priority of 5 which is NOT within the given range
+        assertEquals(0, check.check(cE, cD)); // cD has a preference of 7 which is NOT within the range and higher than cE's
     }
 
 }
