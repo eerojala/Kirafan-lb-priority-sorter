22de80926850471e48855e784bb80726a3ecd559
eerojala <eero.ojala@helsinki.fi>
2021-01-05 15:12:06 +0200

    Added skeleton GUI and controllers for adding/editing characters and skills

---
+++ kirafan-lb-priority-sorter/src/main/java/logic/controllers/CharacterWindowController.java
@@ -0,0 +1,64 @@
+package logic.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class CharacterWindowController {
+    @FXML
+    private TextField textFieldName;
+
+    @FXML
+    private ComboBox<?> cmbBoxSeries;
+
+    @FXML
+    private ComboBox<?> cmbBoxElement;
+
+    @FXML
+    private ComboBox<?> cmbBoxClass;
+
+    @FXML
+    private TextField textFieldWokeLevel;
+
+    @FXML
+    private TextField textFieldOffensiveStat;
+
+    @FXML
+    private TextField textFieldDEF;
+
+    @FXML
+    private TextField textFieldMDF;
+
+    @FXML
+    private ComboBox<?> cmbBoxWeapon;
+
+    @FXML
+    private ListView<?> listViewSkills;
+
+    @FXML
+    private Button buttonAddSkill;
+
+    @FXML
+    private TextField textFieldPersonalPreference;
+
+    @FXML
+    private CheckBox checkBoxLimitBroken;
+
+    @FXML
+    private Button buttonSubmit;
+
+    @FXML
+    void handleAddSkillButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleCheckBoxTicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSubmitButtonPressed(ActionEvent event) {
+
+    }
+}
---
+++ kirafan-lb-priority-sorter/src/main/java/logic/controllers/SkillWindowController.java
@@ -0,0 +1,45 @@
+package logic.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class SkillWindowController {
+    @FXML
+    private ComboBox<?> cmbBoxType;
+
+    @FXML
+    private ComboBox<?> cmbBoxChange;
+
+    @FXML
+    private ComboBox<?> cmbBoxTarget;
+
+    @FXML
+    private TextField textFieldPower;
+
+    @FXML
+    private Button buttonSubmit;
+
+    @FXML
+    void handleChangeComboBox(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSubmitButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleTargetComboBox(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleTypeComboBox(ActionEvent event) {
+
+    }
+
+}
